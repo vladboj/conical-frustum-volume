42141d9987cd5d5ae89f833b91143a18890ac730
Vlad Bojan <vlad.bojan0@gmail.com>
2024-03-03 13:54:59 +0200

    add ability to run the volume_computation by itself

--- volume_computation.py
+++ volume_computation.py
@@ -9,3 +9,12 @@ def compute_volume(a, b, c, d, h):
     volume = math.pi / 3 * ((a * b) * H - (c * d) * (H - h))
 
     return volume
+
+
+def main():
+    print("The volume of the frustum with the given dimension is " +
+          str(round(compute_volume(26, 21, 21, 16, 10) / 1000, 2)) + " liters.")
+
+
+if __name__ == "__main__":
+    main()
