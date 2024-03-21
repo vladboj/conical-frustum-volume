470989ab8da71225c0aa3b023b845ded1bc5acf0
Vlad Bojan <vlad.bojan0@gmail.com>
2024-03-21 18:28:41 +0200

    fix subcontainer volume computation

--- volume_computation.py
+++ volume_computation.py
@@ -14,10 +14,9 @@ def compute_volume(a, b, c, d, h):
 # given the dimensions of the container and a height lower than the original height,
 # i want to compute the volume of the subcontainer with that respective height
 def compute_subcontainer_volume(a, b, c, d, h, new_height):
-    # firstly, compute the scaling factor using the new height in order to scale down the larger base
-    scaling_factor = new_height / h
-    new_base_semi_minor_axis = b * scaling_factor
-    new_base_semi_major_axis = a * scaling_factor
-    new_volume = compute_volume(
-        new_base_semi_major_axis, new_base_semi_minor_axis, c, d, new_height)
+    # i mathematically found the following formula for finding the new minor and major axis
+    # for the new eliptical base
+    new_minor_axis = 2 * d + (new_height * (2 * b - 2 * d)) / h
+    new_major_axis = 2 * c + (new_height * (2 * a - 2 * c)) / h
+    new_volume = compute_volume(new_major_axis / 2, new_minor_axis / 2, c, d, new_height)
     return new_volume
