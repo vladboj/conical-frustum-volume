5a91dcd7cd29dd7fcc66c1c8e94939868dfa7270
Vlad Bojan <vlad.bojan0@gmail.com>
2024-04-16 18:21:00 +0300

    replace hardcoded path with dynamic value

--- inputs_getter.py
+++ inputs_getter.py
@@ -1,9 +1,12 @@
+import os
 from PIL import Image
 
 
 def get_inputs():
     # show image for user so that he understands what each input means
-    img = Image.open("./assets/eliptical_frustum.png")
+    script_directory = os.path.dirname(__file__)
+    image_path = os.path.join(script_directory, "assets", "eliptical_frustum.png")
+    img = Image.open(image_path)
     img.show()
 
     # place inputs in a dictionary
